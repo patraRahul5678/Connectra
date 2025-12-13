c27bc1633cb73b7e6b745575f0e0de08b8e6f224
patraRahul5678 <kanhapatra801@gmail.com>
2025-12-13 13:36:21 +0530

    /

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://streamify-10.onrender.com";
+const BASE_URL ="https://streamify-10.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
